2744a6247d29a33e52e4f3b2c02fd87b1d9448bd
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-04-10 20:42:44 +0900

    Implement Product routes

---
+++ go-grpc-api-gateway/pkg/product/register.go
@@ -0,0 +1,27 @@
+package product
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hirasawayuki/go-grpc-project/go-grpc-api-gateway/pkg/auth"
+	"github.com/hirasawayuki/go-grpc-project/go-grpc-api-gateway/pkg/config"
+	"github.com/hirasawayuki/go-grpc-project/go-grpc-api-gateway/pkg/product/routes"
+)
+
+func RegisterRoutes(r *gin.Engine, c *config.Config, authSvc *auth.ServiceClient) {
+	a := auth.InitAuthMiddleware(authSvc)
+	svc := &ServiceClient{
+		Client: InitServiceClient(c),
+	}
+
+	routes := r.Group("/product")
+	routes.Use(a.AuthRequired)
+	routes.POST("/", svc.FindOne)
+}
+
+func (svc *ServiceClient) FindOne(ctx *gin.Context) {
+	routes.FindOne(ctx, svc.Client)
+}
+
+func (svc *ServiceClient) CreateProduct(ctx *gin.Context) {
+	routes.CreateProduct(ctx, svc.Client)
+}
