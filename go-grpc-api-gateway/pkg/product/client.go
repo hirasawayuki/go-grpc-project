2526c6e93245964023983cb64286a41711891f8d
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-04-10 20:42:01 +0900

    Create ProductServiceClient

---
+++ go-grpc-api-gateway/pkg/product/client.go
@@ -0,0 +1,22 @@
+package product
+
+import (
+	"fmt"
+
+	"github.com/hirasawayuki/go-grpc-project/go-grpc-api-gateway/pkg/config"
+	"github.com/hirasawayuki/go-grpc-project/go-grpc-api-gateway/pkg/product/pb"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.ProductServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.ProductServiceClient {
+	cc, err := grpc.Dial(c.ProductSvcUrl, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Could not connect: ", err)
+	}
+
+	return pb.NewProductServiceClient(cc)
+}
